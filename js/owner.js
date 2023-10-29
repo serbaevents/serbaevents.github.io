6ea6527d8b41f5e40322548d01e819cf281667b1
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2023-10-29 11:45:21 +0700

    test

---
+++ js/owner.js
@@ -0,0 +1,9 @@
+import data from "../json/owner.json" assert { type: "json" };
+import { setInner } from "https://jscroot.github.io/element/croot.js";
+
+setInner("namasaw", data.owner.namasaw);
+setInner("npmsaw", data.owner.npmsaw);
+setInner("namazar", data.owner.namazar);
+setInner("npmzar", data.owner.npmzar);
+setInner("akungithub", data.owner.akungithub);
+setInner("akungithub2", data.owner.akungithub2);
