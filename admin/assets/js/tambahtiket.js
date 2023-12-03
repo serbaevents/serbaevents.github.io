8238ce025ba9ba76570e55785767dffdbf8488b3
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-12-04 02:50:25 +0800

    main

---
+++ admin/assets/js/tambahtiket.js
@@ -0,0 +1,40 @@
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import { getValue } from "https://jscroot.github.io/element/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+const postTambahTiket = () => {
+  const target_url =
+    "https://asia-southeast2-proven-wavelet-401905.cloudfunctions.net/getdataevent";
+  const tokenvalue = getCookie("Authorization");
+  const tokenkey = "Authorization";
+  const datainjson = {
+    tujuaneven: getValue("tujuaneven"),
+    jemputan: getValue("jemputan"),
+    keterangan: getValue("keterangan"),
+    harga: getValue("harga"),
+  };
+  postWithToken(target_url, tokenkey, tokenvalue, datainjson, responseData);
+  console.log(datainjson);
+};
+
+const responseData = (result) => {
+  if (result.status) {
+    Swal.fire({
+      icon: "success",
+      title: "Insert Successful",
+      text: result.message,
+    }).then(() => {
+      window.location.reload();
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Insert Failed",
+      text: result.message,
+    });
+  }
+};
+
+window.postTambahTiket = postTambahTiket;
+
+
