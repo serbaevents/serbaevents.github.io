8a3a7056a28dcdee8ca6a9e40e58f75a8a5cec0e
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2023-12-05 14:08:26 +0700

    tes

--- admin/assets/js/getTiketfunc.js
+++ admin/assets/js/getTiketfunc.js
@@ -1,4 +1,4 @@
-import { URLGetEvent, responseData } from "./getTiket.js";
+import { URLGetEvent, responseData } from "../js/getTiket.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { hide } from "https://jscroot.github.io/element/croot.js";
 
